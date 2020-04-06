52f3b80727795b6ae13fb44160bdda7d16294b79
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-06 14:20:23 +0600

    init list view

--- app/src/main/java/com/luminous/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/luminous/notekeeper/NoteListActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
 
 public class NoteListActivity extends AppCompatActivity {
 
@@ -29,7 +30,7 @@ public class NoteListActivity extends AppCompatActivity {
     }
 
     private void initializeDisplayContent() {
-        
+        ListView listNotes = findViewById(R.id.listNotes);
     }
 
 
