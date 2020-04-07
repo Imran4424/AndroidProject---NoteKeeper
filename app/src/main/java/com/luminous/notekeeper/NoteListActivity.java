981b18c716e1412face7453dad14d619ce03a807
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-07 21:21:06 +0600

    set note item click interactio9n

--- app/src/main/java/com/luminous/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/luminous/notekeeper/NoteListActivity.java
@@ -43,7 +43,7 @@ public class NoteListActivity extends AppCompatActivity {
         listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                
+
             }
         });
     }
