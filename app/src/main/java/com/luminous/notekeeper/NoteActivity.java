862746acd3cbac9daa21dcbaf53e5f32ffc09b17
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-10 22:29:17 +0600

    work with intent extra

--- app/src/main/java/com/luminous/notekeeper/NoteActivity.java
+++ app/src/main/java/com/luminous/notekeeper/NoteActivity.java
@@ -14,7 +14,7 @@ import android.widget.Spinner;
 import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
-    public static final String noteInfo = "";
+    public static final String noteInfo = "NOTE_INFO";
     Spinner spinnerCourses;
 
     @Override
