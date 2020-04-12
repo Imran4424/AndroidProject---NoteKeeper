96f78c73e1ff3c2cf35b160f494293f128f90efa
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-12 10:47:07 +0600

    create new node

--- app/src/main/java/com/luminous/notekeeper/NoteActivity.java
+++ app/src/main/java/com/luminous/notekeeper/NoteActivity.java
@@ -67,6 +67,10 @@ public class NoteActivity extends AppCompatActivity {
         }
     }
 
+    private void createNewNode() {
+    
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
