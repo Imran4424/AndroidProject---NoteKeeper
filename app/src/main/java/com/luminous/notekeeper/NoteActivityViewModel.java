a49bec48e4f5f6f3b446088dc2add9741c6282bc
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-13 13:59:00 +0600

    viewModel restore state

--- app/src/main/java/com/luminous/notekeeper/NoteActivityViewModel.java
+++ app/src/main/java/com/luminous/notekeeper/NoteActivityViewModel.java
@@ -10,7 +10,7 @@ public class NoteActivityViewModel extends ViewModel {
     public String originalNoteCourseId;
     public String originalNoteTitle;
     public String originalNoteBody;
-    public boolean isNewlyCreated;
+    public boolean isNewlyCreated = true;
 
     public void saveState(Bundle outState) {
         outState.putString(ORIGINAL_NOTE_COURSE_ID, originalNoteCourseId);
