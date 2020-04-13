4a707ec9a97d16d17340b63a6290b07b479db096
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-13 13:46:52 +0600

    viewModel save state

--- app/src/main/java/com/luminous/notekeeper/NoteActivityViewModel.java
+++ app/src/main/java/com/luminous/notekeeper/NoteActivityViewModel.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class NoteActivityViewModel extends ViewModel {
     public static final String ORIGINAL_NOTE_COURSE_ID = "com.luminous.notekeeper.ORIGINAL_NOTE_COURSE_ID";
+    public static final String ORIGINAL_NOTE_TITLE = "com.luminous.notekeeper.ORIGINAL_NOTE_TITLE";
+    public static final String ORIGINAL_NOTE_BODY = "com.luminous.notekeeper.ORIGINAL_NOTE_BODY";
     public String originalNoteCourseId;
     public String originalNoteTitle;
     public String originalNoteBody;
