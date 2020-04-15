0baa046e65ac98e72914f4bfd841e2a1da34b73e
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-15 17:13:14 +0600

    set up DataManager Test

--- app/src/test/java/com/luminous/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/luminous/notekeeper/DataManagerTest.java
@@ -8,6 +8,6 @@ public class DataManagerTest {
 
     @Test
     public void createNewNote() {
-        
+        final CourseInfo course = DataManager.getInstance().getCourse("android_async");
     }
 }
