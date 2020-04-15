a574eccd21189ba6e9a57c53b00429cad66413d1
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-15 22:11:31 +0600

    set up DataManager Test

--- app/src/test/java/com/luminous/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/luminous/notekeeper/DataManagerTest.java
@@ -15,5 +15,7 @@ public class DataManagerTest {
 
         int noteIndex = dm.createNewNote();
         NoteInfo newNote = dm.getNotes().get(noteIndex);
+        newNote.setCourse(course);
+        newNote.setTitle(noteTitle);
     }
 }
