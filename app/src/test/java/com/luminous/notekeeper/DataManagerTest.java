af0340a6dbfdbb097d61cfc37f607583f44c2357
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-16 00:20:58 +0600

    find similar notes unit test

--- app/src/test/java/com/luminous/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/luminous/notekeeper/DataManagerTest.java
@@ -48,5 +48,6 @@ public class DataManagerTest {
         assertEquals(noteIndexOne, foundIndexOne);
 
         int foundIndexTwo = dm.findNote(newNoteTwo);
+        assertEquals(noteIndexTwo, foundIndexTwo);
     }
 }
