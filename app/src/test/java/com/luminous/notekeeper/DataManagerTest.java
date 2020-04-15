854b21c2fa00bb9f3ac8e35089ca54ac0434a8f3
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-16 02:14:30 +0600

    pre-processing unit test

--- app/src/test/java/com/luminous/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/luminous/notekeeper/DataManagerTest.java
@@ -21,7 +21,6 @@ public class DataManagerTest {
 
     @Test
     public void createNewNote() {
-        DataManager dm = DataManager.getInstance();
         final CourseInfo course = dm.getCourse("android_async");
         final  String noteTitle = "Test note title";
         final  String noteBody = "Test note body";
