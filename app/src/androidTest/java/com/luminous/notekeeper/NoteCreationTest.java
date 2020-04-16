bd7a5788a9401434456bb7dd69ac3611cc4fdd12
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-16 21:09:42 +0600

    set note Creation instrumental test

--- app/src/androidTest/java/com/luminous/notekeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/luminous/notekeeper/NoteCreationTest.java
@@ -1,7 +1,12 @@
 package com.luminous.notekeeper;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
 import static org.junit.Assert.*;
 
+@RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
 
 }
