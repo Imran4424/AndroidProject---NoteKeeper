974c33e47b299c5d31d4675d2cb80a30aeeae421
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-16 21:18:55 +0600

    set note Creation instrumental test

--- app/src/androidTest/java/com/luminous/notekeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/luminous/notekeeper/NoteCreationTest.java
@@ -3,12 +3,14 @@ package com.luminous.notekeeper;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
+    @Rule
     public ActivityTestRule <NoteListActivity> noteListActivityActivityTestRule =
             new ActivityTestRule<>(NoteListActivity.class);
 }
