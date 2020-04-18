b75be781cfd2317767a3bab7ed14a5e5acb72d25
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-18 22:28:16 +0600

    set recycler adapter for navigaton activity

--- app/src/main/java/com/luminous/notekeeper/MainActivity.java
+++ app/src/main/java/com/luminous/notekeeper/MainActivity.java
@@ -17,7 +17,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private NoteRecyclerAdapter noteRecyclerAdapter;
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
