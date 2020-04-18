6ff283192f94f063184d7daf658b9d2a50357458
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-18 11:16:07 +0600

    class Note Recycler Adapter

--- app/src/main/java/com/luminous/notekeeper/NoteRecyclerAdapter.java
+++ app/src/main/java/com/luminous/notekeeper/NoteRecyclerAdapter.java
@@ -1,28 +1,11 @@
 package com.luminous.notekeeper;
 
 import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class NoteRecyclerAdapter extends RecyclerView.Adapter {
-
-    @NonNull
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
+public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
