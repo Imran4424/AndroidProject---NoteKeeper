6b656f1c17d54db28c966d65909a1cd6e37bf93a
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-19 10:59:17 +0600

    set up naviatoin drawer intent

--- app/src/main/java/com/luminous/notekeeper/MainActivity.java
+++ app/src/main/java/com/luminous/notekeeper/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 startActivity(new Intent(MainActivity.this, NoteActivity.class));
             }
         });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -51,6 +52,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(this);
 
         initializeDisplayContent();
     }
