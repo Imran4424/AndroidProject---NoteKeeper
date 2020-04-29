1f155e12a7f7c719e86575e458429a2afb25531e
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-29 13:45:23 +0600

    add course Recycler adapter

---
+++ app/src/main/java/com/luminous/notekeeper/CourseRecyclerAdapter.java
@@ -0,0 +1,67 @@
+package com.luminous.notekeeper;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class CourseRecyclerAdapter extends RecyclerView.Adapter<CourseRecyclerAdapter.ViewHolder> {
+    private final Context context;
+    private final LayoutInflater layoutInflater;
+    private final List<NoteInfo> notes;
+
+    public CourseRecyclerAdapter(Context context, List<NoteInfo> notes) {
+        this.context = context;
+        layoutInflater = LayoutInflater.from(context);
+        this.notes = notes;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = layoutInflater.inflate(R.layout.item_note_list, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        NoteInfo note = notes.get(position);
+        holder.textCourse.setText(note.getCourse().getTitle());
+        holder.textTitle.setText(note.getTitle());
+        holder.currentPosition = position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public final TextView textCourse;
+        public final TextView textTitle;
+        public int currentPosition;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textCourse = (TextView) itemView.findViewById(R.id.textCourse);
+            textTitle = (TextView) itemView.findViewById(R.id.textTitle);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent noteIntent = new Intent(context, NoteActivity.class);
+                    noteIntent.putExtra(NoteActivity.NOTE_POSITION, currentPosition);
+                    context.startActivity(noteIntent);
+                }
+            });
+        }
+    }
+}
